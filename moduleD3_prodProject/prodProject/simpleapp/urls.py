7fbac10cae67c31f5beadf175254a9f05bc19a09
VetN <122635397+VetN@users.noreply.github.com>
2023-07-06 09:56:11 +0300

    add

--- moduleD3_prodProject/prodProject/simpleapp/urls.py
+++ moduleD3_prodProject/prodProject/simpleapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 #from django.views import View
 from .views import ProductsList, ProductDetail
 #from .views import  ProductDetail, View
@@ -7,8 +7,9 @@ from .views import ProductsList, ProductDetail
 urlpatterns = [
    
     path('', ProductsList.as_view()),# т.к.это класс, то надо представить этот класс в виде view. Для этого вызываем метод as_view
-    path('products', ProductsList.as_view()),
-    path('product/<int:pk>', ProductDetail.as_view()),
+    #path('products', ProductsList.as_view()),
+    path('product/<int:pk>', ProductDetail.as_view(), name='product'),
+    re_path(r'^products', ProductsList.as_view(), name = 'products') # маршрут позвол по-разному писать адрес при вызове
 ]
     # если прописываем просто через View
 
